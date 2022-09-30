bb1c4bf3b213cfcbb19d8459f8fdaec24bea0287
Barry Hanna <barryhanna@users.noreply.github.com>
2022-09-30 21:14:46 +0100

    First Template

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php
 // Keep this opening tag!
+
+$title = 'PHP Fun';
+$heading = 'Our first post';
+$content = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat reprehenderit quasi veritatis quisquam numquam distinctio id rem debitis omnis voluptatibus tenetur, doloribus odit maxime aliquam necessitatibus repellendus iste neque quo soluta inventore voluptas vitae ea. Dolores eos, quod ad ipsum ullam quis suscipit, explicabo nihil non consequatur cum! Earum, voluptatibus!';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo $title; ?></title>
+</head>
+<body>
+	<h1><?php echo $heading; ?></h1>
+	<p><?php echo $content; ?></p>
+</body>
+</html>
